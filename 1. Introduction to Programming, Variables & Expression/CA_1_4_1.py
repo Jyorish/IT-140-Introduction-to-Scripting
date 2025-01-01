348172a8fdeb31943630bac8233e986c1af5a029
Jyorish Sainju <125849581+Jyorish@users.noreply.github.com>
2025-01-01 00:46:53 -0700

    1.4 Errors

---
+++ 1. Introduction to Programming, Variables & Expression/CA_1_4_1.py
@@ -0,0 +1,22 @@
+# Course = IT 140 - Introduction to Scripting
+# Southern New Hampshire University
+# Module 1 - Introduction to Programming, Variables & Expression
+# 1.4 Errors
+# Channlege Activity 1.4.1: Basic syntax errors.
+
+# Retype the statements, correcting the syntax error in each print statements.
+
+# print('Predictions are hard.")
+# print(Especially about the future.)
+# user_num = 5
+# print('user_num is: ' user_num)
+
+# mismatch quotes, use either ' or " on both sides of the text
+print('Predictions are hard.')
+
+# missing quotes, put ' or " on both side of the text
+print('Especially about the future.')
+user_num = 5
+
+# missing comma between the statement and the variable
+print('user_num is: ', user_num)
